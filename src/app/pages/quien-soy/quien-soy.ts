b476d6a69051662d2281d28a118f74a89dec8b2a
Mpiadeveloper <mpiadeveloper@gmail.com>
2026-05-18 14:32:24 -0300

    Sprint 1: componentes, quien-soy, favicon, etc.

--- src/app/pages/quien-soy/quien-soy.ts
+++ src/app/pages/quien-soy/quien-soy.ts
@@ -1,18 +1,23 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, ChangeDetectorRef } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-quien-soy',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './quien-soy.html',
   styleUrl: './quien-soy.css',
 })
 export class QuienSoy {
   http = inject(HttpClient);
-  user: any;
+  cdr = inject(ChangeDetectorRef);
+  user: any = null;
 
   ngOnInit() {
-    this.http.get('https://api.github.com/users//franetovichpia').subscribe(data => this.user = data);
+    this.http.get('https://api.github.com/users/franetovichpia').subscribe(data => {
+      this.user = data;
+      this.cdr.detectChanges();
+    });
   }
 }
